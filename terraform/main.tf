d0e1bfca0559e3f3c19f54bfec24b6d7fcd61d53
Vinayak Sharma <83303847+Vinayak-009@users.noreply.github.com>
2024-07-04 00:56:39 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,30 +7,73 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
-  initial_node_count = 2
+  remove_default_node_pool = true
+  initial_node_count       = 1
+
+
 
   node_config {
     machine_type = "e2-medium"
-  }
-}
-
-resource "google_container_node_pool" "primary_nodes" {
-  cluster    = google_container_cluster.primary.name
-  location   = google_container_cluster.primary.location
-  node_count = 2
-
-  node_config {
-    preemptible  = false
-    machine_type = "e2-medium"
+    disk_type    = "pd-balanced"
+    disk_size_gb = 10
     oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
+    service_account = var.service_account
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
   }
-}
 
-resource "local_file" "kubeconfig" {
-  content  = google_container_cluster.primary.endpoint
-  filename = "${path.module}/kubeconfig"
+  ip_allocation_policy {
+    use_ip_aliases = true
+  }
+
+  master_authorized_networks_config {
+    cidr_blocks = []
+  }
+
+  addons_config {
+    http_load_balancing {
+      disabled = false
+    }
+
+
+    gce_persistent_disk_csi_driver_config {
+      enabled = true
+    }
+  }
+
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+
+  release_channel {
+    channel = "REGULAR"
+  }
+
+  node_pool {
+    name       = "default-pool"
+    node_count = 2
+    node_config {
+      machine_type = "e2-medium"
+      disk_type    = "pd-balanced"
+      disk_size_gb = 10
+      oauth_scopes = [
+        "https://www.googleapis.com/auth/cloud-platform",
+      ]
+      service_account = var.service_account
+      metadata = {
+        disable-legacy-endpoints = "true"
+      }
+    }
+  }
+
+  network    = "projects/${var.project_id}/global/networks/default"
+  subnetwork = "projects/${var.project_id}/regions/${var.region}/subnetworks/default"
+
+  shielded_nodes {
+    enabled = true
+  }
 }
 
 output "kubeconfig_file_path" {
