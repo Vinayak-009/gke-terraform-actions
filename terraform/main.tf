78d3d90f40aadac508b4a6d95896bc01a3b50cdd
Vinayak Sharma <83303847+Vinayak-009@users.noreply.github.com>
2024-07-04 00:12:55 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,4 +14,25 @@ resource "google_container_cluster" "primary" {
   }
 }
 
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  node_count = 3
 
+  node_config {
+    preemptible  = false
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
+
+resource "local_file" "kubeconfig" {
+  content  = google_container_cluster.primary.endpoint
+  filename = "${path.module}/kubeconfig"
+}
+
+output "kubeconfig_file_path" {
+  value = local_file.kubeconfig.filename
+}
