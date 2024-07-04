50b0441e206b8f378f337c5a3dd2c0c1fca44df6
Vinayak Sharma <83303847+Vinayak-009@users.noreply.github.com>
2024-07-04 15:10:06 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -21,6 +21,35 @@ variable "cluster_name" {
   default     = "gke-cluster"
 }
 
+
+
+variable "machine_type" {
+  type        = string
+  description = "Type of the node compute engines."
+}
+
+
+
+
+variable "min_count" {
+  type        = number
+  description = "Minimum number of nodes in the NodePool. Must be >=0 and <= max_node_count."
+}
+
+variable "max_count" {
+  type        = number
+  description = "Maximum number of nodes in the NodePool. Must be >= min_node_count."
+}
+
+variable "disk_size_gb" {
+  type        = number
+  description = "Size of the node's disk."
+}
+
+variable "initial_node_count" {
+  type        = number
+  description = "The number of nodes to create in this cluster's default node pool."
+}
 variable "service_account" {
   description = "The service account email for the nodes"
   type        = string
