77a889bbeddbc2fea6763da129c07ea3d61deeda
Vinayak Sharma <83303847+Vinayak-009@users.noreply.github.com>
2024-07-05 01:33:03 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,52 +1,23 @@
 variable "project_id" {
-  description = "kube-projects-427711"
-  type        = string
+  description = "The project ID of your Google Cloud project"
+}
+
+variable "cluster_name" {
+  description = "The name for the GKE cluster"
+  default     = "kube-cluster"
 }
 
 variable "region" {
-  description = "The GCP region"
-  type        = string
+  description = "The region to deploy the GKE cluster in"
   default     = "us-central1"
 }
 
 variable "zone" {
-  description = "The GCP zone"
-  type        = string
+  description = "The zone within the region to deploy the GKE cluster in"
   default     = "us-central1-c"
 }
 
-variable "cluster_name" {
-  description = "kube-cluster"
-  type        = string
-  default     = "gke-cluster"
-}
-
-variable "machine_type" {
-  type        = string
-  description = "Type of the node compute engines."
-}
-
-variable "min_count" {
-  type        = number
-  description = "Minimum number of nodes in the NodePool. Must be >=0 and <= max_node_count."
-}
-
-variable "max_count" {
-  type        = number
-  description = "Maximum number of nodes in the NodePool. Must be >= min_node_count."
-}
-
-variable "disk_size_gb" {
-  type        = number
-  description = "Size of the node's disk."
-}
-
-variable "initial_node_count" {
-  type        = number
-  description = "The number of nodes to create in this cluster's default node pool."
-}
-
 variable "service_account" {
-  description = "The service account email for the nodes"
-  type        = string
+  description = "The service account to associate with GKE nodes"
+  default     = "gke-terraform-actions@kube-projects-427711.iam.gserviceaccount.com"
 }
