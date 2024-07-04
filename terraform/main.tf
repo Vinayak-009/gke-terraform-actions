8ef8ab8854291568a626c1c00cb2f14d16577dae
Vinayak Sharma <83303847+Vinayak-009@users.noreply.github.com>
2024-07-05 01:30:23 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,74 +3,57 @@ provider "google" {
   region  = var.region
 }
 
-module "gke" {
-  source      = "terraform-google-modules/kubernetes-engine/google"
-  version     = ">= 4.1.0"
-  project_id  = var.project_id
-  region      = var.region
-  zones       = var.zones
-  name        = var.name
-  network     = "default"
-  subnetwork  = "default"
-  horizontal_pod_autoscaling = true
-  node_pools = [
-    {
-      name               = "default-node-pool"
-      machine_type       = var.machine_type
-      min_count          = var.min_count
-      max_count          = var.max_count
-      disk_size_gb       = var.disk_size_gb
-      disk_type          = "pd-balanced"
-      image_type         = "COS"
-      auto_repair        = true
-      auto_upgrade       = true
-      service_account    = var.service_account
-      preemptible        = false
-      initial_node_count = var.initial_node_count
-    },
-  ]
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.zone
 
-  node_pools_oauth_scopes = {
-    all = []
+  remove_default_node_pool = true
+  initial_node_count       = 1
 
-    "default-node-pool" = [
+  node_config {
+    machine_type = "e2-medium"
+    disk_type    = "pd-balanced"
+    disk_size_gb = 10
+    oauth_scopes = [
       "https://www.googleapis.com/auth/cloud-platform",
     ]
-  }
-
-  node_pools_labels = {
-    all = {}
-
-    "default-node-pool" = {
-      default-node-pool = true
+    service_account = var.service_account
+    metadata = {
+      disable-legacy-endpoints = "true"
     }
   }
 
-  node_pools_metadata = {
-    all = {}
+  logging_service    = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
 
-    "default-node-pool" = {
-      node-pool-metadata-custom-value = "my-node-pool"
-    }
+  release_channel {
+    channel = "REGULAR"
   }
 
-  node_pools_taints = {
-    all = []
+  # Optional: Network configuration
+  network    = "projects/${var.project_id}/global/networks/default"
+  subnetwork = "projects/${var.project_id}/regions/${var.region}/subnetworks/default"
+}
 
-    "default-node-pool" = [
-      {
-        key    = "default-node-pool"
-        value  = "true"
-        effect = "PREFER_NO_SCHEDULE"
-      },
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  name       = "primary-node-pool"
+  node_count = 2
+
+  node_config {
+    machine_type = "e2-medium"
+    disk_type    = "pd-balanced"
+    disk_size_gb = 10
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
     ]
+    service_account = var.service_account
   }
+}
 
-  node_pools_tags = {
-    all = []
-
-    "default-node-pool" = [
-      "default-node-pool",
-    ]
-  }
+# Data source to fetch information about the cluster
+data "google_container_cluster" "cluster" {
+  name     = google_container_cluster.primary.name
+  location = google_container_cluster.primary.location
 }
